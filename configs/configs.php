83c5b6883c747843b3c76b31f8e7519150c807da
unknown <alexsrk09@gmail.com>
2022-11-04 19:58:28 +0100

    configs
    muevo el array del mapa a configs.php

---
+++ configs/configs.php
@@ -0,0 +1,175 @@
+<?php
+$map = array(
+    "l1" => array(
+        "z1" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => true,
+                "S" => false,
+                "E" => false,
+                "W" => true,
+            ),
+        ),
+        "z2" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => false,
+                "E" => true,
+                "W" => true,
+            ),
+        ),
+        "z3" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => true,
+                "E" => true,
+                "W" => false,
+            ),
+        ),
+    ),
+    "l2" => array(
+        "z1" => array(
+            'vacio' => true,
+        ),
+        "z2" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => true,
+                "E" => false,
+                "W" => true,
+            ),
+        ),
+        "z3" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => true,
+                "S" => false,
+                "E" => true,
+                "W" => false,
+            ),
+        ),
+    ),
+    "l3" => array(
+        "z1" => array(
+            "vacio" => true,
+        ),
+        "z2" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => true,
+                "S" => true,
+                "E" => false,
+                "W" => false,
+            ),
+        ),
+    ),
+    "l4" => array(
+        "z1" => array(
+            "vacio" => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => false,
+                "E" => false,
+                "W" => true,
+            ),
+        ),
+        "z2" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => true,
+                "S" => false,
+                "E" => true,
+                "W" => true,
+            ),
+        ),
+        "z3" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => false,
+                "E" => true,
+                "W" => true,
+            ),
+        ),
+        "z4" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => true,
+                "E" => true,
+                "W" => false,
+            ),
+        ),
+    ),
+    "l5" => array(
+        "z1" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => false,
+                "E" => false,
+                "W" => true,
+            ),
+        ),
+        "z2" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => false,
+                "E" => true,
+                "W" => true,
+            ),
+        ),
+        "z3" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => false,
+                "S" => false,
+                "E" => true,
+                "W" => true,
+            ),
+        ),
+        "z4" => array(
+            'vacio' => false,
+            'enemigos' => array(
+            ),
+            'puertas' => array(
+                "N" => true,
+                "S" => false,
+                "E" => true,
+                "W" => false,
+            ),
+        ),
+    ),
+);
+
+?>
